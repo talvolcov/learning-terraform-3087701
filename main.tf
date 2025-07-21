0be8ebd181bcf3c7c6ec6d780ca1ca43cf4f551b
talvolcov <54931709+talvolcov@users.noreply.github.com>
2025-07-21 18:18:11 +0200

    Fix bug new vpc

--- main.tf
+++ main.tf
@@ -27,7 +27,6 @@ module "blog_vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Terraform = "true"
@@ -41,7 +40,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "Learning Terraform"
   }
