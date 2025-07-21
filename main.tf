ee0f0f00d7d0b4661c77d8ad018ef9c82aeb8c29
talvolcov <54931709+talvolcov@users.noreply.github.com>
2025-07-21 13:01:40 +0200

    Fix bugs security groups

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_id = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
-  from_Port         = 443
+  from_port         = 443
   to_port           = 443
   protocol          = "tcp" 
   cidr_blocks       = ["0.0.0.0/0"]
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 
 resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
-  from_Port         = 80
+  from_port         = 80
   to_port           = 80
   protocol          = "tcp" 
   cidr_blocks       = ["0.0.0.0/0"]
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 resource "aws_security_group_rule" "blog_http_out" {
   type = "egress"
-  from_Port         = 0
+  from_port         = 0
   to_port           = 0
   protocol          = "-1" 
   cidr_blocks       = ["0.0.0.0/0"]
