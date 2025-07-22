027b50a75d1f8f0199410347b3bac70054fb3ed3
talvolcov <54931709+talvolcov@users.noreply.github.com>
2025-07-22 18:17:21 +0200

    try LB

--- main.tf
+++ main.tf
@@ -76,13 +76,13 @@ module "alb" {
     }
   ]
 
- http_tcp_listeners = [
+ http_tcp_listener = 
   {
       port     = 80
       protocol = "HTTP"
       target_group_index = 0
     }
- ]
+ 
   tags = {
     Environment = "dev"
   }
